b8dba65559cd8ce7c907893625009a03c57fc08e
0xb612f <0xb612f@gmail.com>
2023-03-26 13:53:00 +0900

    basic wireframe cube and camera w/o lighting

--- main.cpp
+++ main.cpp
@@ -1,143 +1,161 @@
-#include <bits/stdc++.h>
 #include <GL/glut.h>
-using namespace std;
+#include <cmath>
 
-// Global variables for camera position and direction
-float cameraX = 0.0, cameraY = 0.0, cameraZ = 5.0;
-float cameraAngleX = 0.0, cameraAngleY = 0.0;
+// Cube vertices
+GLfloat vertices[][3] = {
+  {-1.0,-1.0,-1.0},
+  {1.0,-1.0,-1.0},
+  {1.0,1.0,-1.0},
+  {-1.0,1.0,-1.0},
+  {-1.0,-1.0,1.0},
+  {1.0,-1.0,1.0},
+  {1.0,1.0,1.0},
+  {-1.0,1.0,1.0}
+};
 
-// Global variables for light position and direction
-float lightX = 0.0, lightY = 5.0, lightZ = 5.0;
-float lightAngleX = -45.0, lightAngleY = 45.0;
+// Cube edges
+GLubyte edges[][2] = {
+  {0,1},
+  {1,2},
+  {2,3},
+  {3,0},
+  {4,5},
+  {5,6},
+  {6,7},
+  {7,4},
+  {0,4},
+  {1,5},
+  {2,6},
+  {3,7}
+};
 
-// Draw a cube with lighting
-void drawCubeWithLight() {
-    // Set material properties
-    GLfloat mat_ambient[] = { 0.5, 0.5, 0.5, 1.0 };
-    GLfloat mat_diffuse[] = { 0.5, 0.5, 0.5, 1.0 };
-    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat mat_shininess[] = { 50.0 };
-    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+// Camera position and rotation
+GLfloat camX = 0.0, camY = 0.0, camZ = 5.0;
+GLfloat camYaw = 0.0, camPitch = 0.0;
 
-    // Set light properties
-    GLfloat light_position[] = { lightX, lightY, lightZ, 1.0 };
-    GLfloat light_ambient[] = { 0.2, 0.2, 0.2, 1.0 };
-    GLfloat light_diffuse[] = { 0.5, 0.5, 0.5, 1.0 };
-    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-
-    // Enable lighting
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-
-    // Draw cube
-    glutSolidCube(1.0);
-
-    // Disable lighting
-    glDisable(GL_LIGHTING);
-}
-
-// Display function
 void display() {
-    // Clear screen and depth buffer
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // Set camera position and direction
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(cameraX, cameraY, cameraZ, cameraX + cos(cameraAngleY * 3.14159 / 180.0), cameraY + sin(cameraAngleX * 3.14159 / 180.0), cameraZ + sin(cameraAngleY * 3.14159 / 180.0), 0.0, 1.0, 0.0);
-
-    // Set light position and direction
-    glPushMatrix();
-    glRotatef(lightAngleY, 0.0, 1.0, 0.0);
-    glRotatef(lightAngleX, 1.0, 0.0, 0.0);
-    GLfloat light_position[] = { lightX, lightY, lightZ, 1.0 };
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    glPopMatrix();
-
-    // Draw cube with lighting
-    drawCubeWithLight();
-    // Swap buffers
-    glutSwapBuffers();
+  // Clear screen
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
+  // Reset modelview matrix
+  glLoadIdentity();
+  
+  // Apply camera transformation
+  glTranslatef(-camX, -camY, -camZ);
+  glRotatef(-camPitch, 1.0, 0.0, 0.0);
+  glRotatef(-camYaw, 0.0, 1.0, 0.0);
+  
+  // Draw cube edges
+  glColor3f(1.0, 1.0, 1.0);
+  glBegin(GL_LINES);
+  for (int i = 0; i < 12; i++) {
+    glVertex3fv(vertices[edges[i][0]]);
+    glVertex3fv(vertices[edges[i][1]]);
+  }
+  glEnd();
+  
+  // Swap buffers
+  glutSwapBuffers();
 }
 
-// Keyboard function for camera movement
-void keyboard(unsigned char key, int x, int y) {
-    switch (key) {
-    case 'w':
-        cameraX += 0.1 * cos(cameraAngleY * 3.14159 / 180.0);
-        cameraZ += 0.1 * sin(cameraAngleY * 3.14159 / 180.0);
-        break;
-    case 's':
-        cameraX -= 0.1 * cos(cameraAngleY * 3.14159 / 180.0);
-        cameraZ -= 0.1 * sin(cameraAngleY * 3.14159 / 180.0);
-        break;
-    case 'a':
-        cameraX -= 0.1 * sin(cameraAngleY * 3.14159 / 180.0);
-        cameraZ += 0.1 * cos(cameraAngleY * 3.14159 / 180.0);
-        break;
-    case 'd':
-        cameraX += 0.1 * sin(cameraAngleY * 3.14159 / 180.0);
-        cameraZ -= 0.1 * cos(cameraAngleY * 3.14159 / 180.0);
-        break;
-    }
-    glutPostRedisplay();
-    cout << cameraX << ' ' << cameraY << ' ' << cameraZ << endl;
+void reshape(int w, int h) {
+  // Set viewport to cover entire window
+  glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+  
+  // Set projection matrix
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(60.0, (GLfloat)w/(GLfloat)h, 0.1, 100.0);
+  
+  // Reset modelview matrix
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 }
 
-// Special keyboard function for camera rotation
-void specialKeyboard(int key, int x, int y) {
-    switch (key) {
-    case GLUT_KEY_UP:
-        cameraAngleX += 5.0;
-        break;
-    case GLUT_KEY_DOWN:
-        cameraAngleX -= 5.0;
-        break;
-    case GLUT_KEY_LEFT:
-        cameraAngleY -= 5.0;
-        break;
-    case GLUT_KEY_RIGHT:
-        cameraAngleY += 5.0;
-        break;
-    }
-    glutPostRedisplay();
-}
-
-// Idle function for light rotation
 void idle() {
-    lightAngleX += 0.5;
-    lightAngleY += 0.5;
-    glutPostRedisplay();
+  // Rotate cube
+  static GLfloat angle = 0.0;
+  angle += 0.5;
+  if (angle > 360.0) {
+    angle -= 360.0;
+  }
+  glRotatef(angle, 0.0, 1.0, 0.0);
+  
+  // Redraw
+  glutPostRedisplay();
 }
 
-// Main function
+void keyboard(unsigned char key, int x, int y) {
+  switch (key) {
+    // Camera movement
+    case 'w':
+      camX -= 0.1 * sin(camYaw * 3.14159265358979323846 / 180.0);
+      camZ += 0.1 * cos(camYaw * 3.14159265358979323846 / 180.0);
+      break;
+    case 's':
+      camX += 0.1 * sin(camYaw * 3.14159265358979323846 / 180.0);
+      camZ -= 0.1 * cos(camYaw * 3.14159265358979323846 / 180.0);
+      break;
+    case 'a':
+      camX -= 0.1 * sin((camYaw - 90.0) * 3.14159265358979323846 / 180.0);
+      camZ += 0.1 * cos((camYaw - 90.0) * 3.14159265358979323846 / 180.0);
+      break;
+    case 'd':
+      camX += 0.1 * sin((camYaw - 90.0) * 3.14159265358979323846 / 180.0);
+      camZ -= 0.1 * cos((camYaw - 90.0) * 3.14159265358979323846 / 180.0);
+      break;
+      
+    // Camera rotation
+    case 'q':
+      camYaw += 2.0;
+      if (camYaw > 360.0) {
+        camYaw -= 360.0;
+      }
+      break;
+    case 'e':
+      camYaw -= 2.0;
+      if (camYaw < 0.0) {
+        camYaw += 360.0;
+      }
+      break;
+    case 'r':
+      camPitch += 2.0;
+      if (camPitch > 90.0) {
+        camPitch = 90.0;
+      }
+      break;
+    case 'f':
+      camPitch -= 2.0;
+      if (camPitch < -90.0) {
+        camPitch = -90.0;
+      }
+      break;
+      
+    // Quit
+    case 27:  // ESC key
+      exit(0);
+      break;
+  }
+}
+
 int main(int argc, char** argv) {
-    // Initialize GLUT
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(640, 480);
-    glutCreateWindow("OpenGL GLUT Camera and Lighting");
+  // Initialize GLUT
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(640, 480);
+  glutCreateWindow("OpenGL Cube");
 
-    // Set up callbacks
-    glutDisplayFunc(display);
-    glutKeyboardFunc(keyboard);
-    glutSpecialFunc(specialKeyboard);
-    glutIdleFunc(idle);
+  // Set up callbacks
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutIdleFunc(idle);
+  glutKeyboardFunc(keyboard);
 
-    // Set up OpenGL
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glShadeModel(GL_SMOOTH);
-    glEnable(GL_DEPTH_TEST);
+  // Enable depth testing
+  glEnable(GL_DEPTH_TEST);
 
-    // Start main loop
-    glutMainLoop();
+  // Start main loop
+  glutMainLoop();
 
-    return 0;
+  return 0;
 }
