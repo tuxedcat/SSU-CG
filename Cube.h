2921eda967edfb8f25dbe3ca9b2b45efdfb7fbe7
wolfrev0 <wolfrevo1337@gmail.com>
2023-05-27 03:15:12 +0900

    impl texture for cube

--- Cube.h
+++ Cube.h
@@ -4,6 +4,70 @@
 class Cube: public GameObject{
 private:
     void onDraw() override{
-        glutSolidCube(1);
+        // glutSolidCube(1);
+
+        glBegin(GL_QUADS);
+
+        // Front face
+        glTexCoord2f(0.0, 0.0);
+        glVertex3f(-0.5, -0.5, 0.5);
+        glTexCoord2f(1.0, 0.0);
+        glVertex3f(0.5, -0.5, 0.5);
+        glTexCoord2f(1.0, 1.0);
+        glVertex3f(0.5, 0.5, 0.5);
+        glTexCoord2f(0.0, 1.0);
+        glVertex3f(-0.5, 0.5, 0.5);
+
+        // Back face
+        glTexCoord2f(1.0, 0.0);
+        glVertex3f(-0.5, -0.5, -0.5);
+        glTexCoord2f(1.0, 1.0);
+        glVertex3f(-0.5, 0.5, -0.5);
+        glTexCoord2f(0.0, 1.0);
+        glVertex3f(0.5, 0.5, -0.5);
+        glTexCoord2f(0.0, 0.0);
+        glVertex3f(0.5, -0.5, -0.5);
+
+        // Top face
+        glTexCoord2f(0.0, 1.0);
+        glVertex3f(-0.5, 0.5, -0.5);
+        glTexCoord2f(0.0, 0.0);
+        glVertex3f(-0.5, 0.5, 0.5);
+        glTexCoord2f(1.0, 0.0);
+        glVertex3f(0.5, 0.5, 0.5);
+        glTexCoord2f(1.0, 1.0);
+        glVertex3f(0.5, 0.5, -0.5);
+
+        // Bottom face
+        glTexCoord2f(1.0, 1.0);
+        glVertex3f(-0.5, -0.5, -0.5);
+        glTexCoord2f(0.0, 1.0);
+        glVertex3f(0.5, -0.5, -0.5);
+        glTexCoord2f(0.0, 0.0);
+        glVertex3f(0.5, -0.5, 0.5);
+        glTexCoord2f(1.0, 0.0);
+        glVertex3f(-0.5, -0.5, 0.5);
+
+        // Right face
+        glTexCoord2f(1.0, 0.0);
+        glVertex3f(0.5, -0.5, -0.5);
+        glTexCoord2f(1.0, 1.0);
+        glVertex3f(0.5, 0.5, -0.5);
+        glTexCoord2f(0.0, 1.0);
+        glVertex3f(0.5, 0.5, 0.5);
+        glTexCoord2f(0.0, 0.0);
+        glVertex3f(0.5, -0.5, 0.5);
+
+        // Left face
+        glTexCoord2f(0.0, 0.0);
+        glVertex3f(-0.5, -0.5, -0.5);
+        glTexCoord2f(1.0, 0.0);
+        glVertex3f(-0.5, -0.5, 0.5);
+        glTexCoord2f(1.0, 1.0);
+        glVertex3f(-0.5, 0.5, 0.5);
+        glTexCoord2f(0.0, 1.0);
+        glVertex3f(-0.5, 0.5, -0.5);
+
+        glEnd();
     }
 };
